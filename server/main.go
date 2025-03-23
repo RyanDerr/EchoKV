396d1b68932edae54defbc4ae655301f4091448a
Ryan Derr <54389874+RyanDerr@users.noreply.github.com>
2025-03-23 16:49:01 -0500

    Rename Project (#12)

--- server/main.go
+++ server/main.go
@@ -7,8 +7,8 @@ import (
 	"net/http"
 	"time"
 
-	service "github.com/RyanDerr/GoKeyValueStore/pkg/service/kv-api"
-	pb "github.com/RyanDerr/GoKeyValueStore/proto-public/go"
+	service "github.com/RyanDerr/EchoKV/pkg/service/kv-api"
+	pb "github.com/RyanDerr/EchoKV/proto-public/go"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
