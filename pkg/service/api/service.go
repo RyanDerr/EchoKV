71cf61ab3b5a37d23474164d0deab9e45d12087c
RyanDerr <JamesRDerr@gmail.com>
2025-04-21 09:46:21 -0500

    Rename API

--- pkg/service/api/service.go
+++ pkg/service/api/service.go
@@ -1,19 +1,28 @@
 package service
 
 import (
+	"log/slog"
+	"os"
+	"sync"
+
 	pb "github.com/RyanDerr/EchoKV/proto-public/go"
+	"github.com/hashicorp/raft"
 )
 
 // Service implements the gRPC service "EchoKV".
 type Service struct {
 	pb.UnimplementedEchoKVServer
-	cache map[string]string
+	store  map[string]string
+	raft   *raft.Raft
+	mu     sync.Mutex
+	logger *slog.Logger
 }
 
 // NewService creates a new Service.
 func NewService() *Service {
 	return &Service{
-		cache: make(map[string]string),
+		store:  make(map[string]string),
+		logger: slog.New(slog.NewTextHandler(os.Stdout, nil)),
 	}
 }
 
